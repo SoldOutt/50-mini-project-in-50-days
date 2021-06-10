e45b99906a02681339482cbe931c28857425b48f
Nguyen Van Nam <boychauphong2001@gmail.com>
2021-06-10 09:17:29 +0700

    hoan thanh bai 3

---
+++ Day3_Rotating-Navication/js/index.js
@@ -0,0 +1,5 @@
+var navigation = document.querySelector('.navigation')
+var container = document.querySelector('.container')
+navigation.addEventListener('click', () => {
+    container.classList.toggle('nav')
+})
