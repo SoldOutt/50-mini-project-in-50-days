e0ca5eedefd7d74e7fbe41f669aaad8f4e0a50c2
Nguyen Van Nam <boychauphong2001@gmail.com>
2021-06-15 10:17:58 +0700

    hoan thanh bai 10

---
+++ Day10_Jokes/index.js
@@ -0,0 +1,15 @@
+var joke = document.querySelector('.box p')
+var api = 'https://icanhazdadjoke.com/'
+var btn = document.querySelector('.box button')
+var head = {
+    headers: {
+        Accecpt: 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+}
+async function getJoke() {
+    await fetch(api, head)
+        .then((response) => response.json())
+        .then((data) => (joke.innerHTML = data.joke))
+}
+btn.addEventListener('click', getJoke)
